8d12395e8049510832db06425b513c88753f7ab5
Doris Ho <doris.h@live.ca>
2019-10-31 10:55:13 -0700

    Started on Login Page

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Login from './comps/Login'
+
+function App(){
+  return (
+    <View>
+      <Login/>
+      </View>
+  )
+}
+
+export default App;
