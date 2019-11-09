bf0e3e11f24d17945912c95a62e97e6573a8f609
Cynthiafeng <43620636+Cynthiafeng@users.noreply.github.com>
2019-11-08 17:11:23 -0800

    Update to help comp

--- styles/HelpStyles.js
+++ styles/HelpStyles.js
@@ -5,20 +5,54 @@ var styles= StyleSheet.create({
     Help:{ 
         justifyContent:"center",
         alignItems: "center",
-        paddingVertical:100,
+        height:"20%",
         backgroundColor: "#FFFEF3"
 
     },
 
     backBtn:{
         width:"50%",
-        height:"50%"
+        height:"50%",
+        left:10
     },
 
     Title:{
         color: "#F04B53",
-        fontSize: 30,
+        fontSize:30,
         textAlign:"center"
+    },
+
+    Faq:{
+        color: "#F58287",
+        fontSize:45,
+        left:20,
+    },
+
+    Htu:{
+        color: "#F58287",
+        fontSize:30,
+        left:20,
+    },
+
+    bodyTxt:{
+
+    },
+
+    htuTxt:{
+
+    },
+
+    img1:{
+
+    },
+    img2:{
+
+    },
+    img3:{
+
+    },
+    img4:{
+
     }
 
 })
