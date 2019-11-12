8bc04766e12f2bf431a8f17f769a51830ceafdcb
Doris Ho <doris.h@live.ca>
2019-11-12 00:49:54 -0800

    Made an attempt with axios, not tested

--- comps/Register.js
+++ comps/Register.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, TextInput, Alert, Button, Image, TouchableOpacity} from 'react-native';
 import styles from '../styles/LoginStyles';
 import {Actions} from 'react-native-router-flux';
+import axios from 'axios';
 
-constructor(props){
+/*constructor(props){
     this.state = {
         UserName: '',
         UserEmail: '',
         UserPassword: ''
     }
-}
+} */
 
-
+var email = "";
+var password = "";
+var name = "";
 
 function Register(){
+    const [error, setError] = useState("");
+    const [users, setUsers] = useState([]);
+
+    const CreateUser = async()=>{
+        var userobj = {
+            key: "create_user",
+            uservalues: {
+                email: email,
+                password: password,
+                name: name
+            }
+        }
+        var data = await axios.post("http://localhost/post", userobj);
+        await ReadUsers();
+    }
+
+    const ReadUsers async()=>{
+        var userobj = {
+            key: "read_users",
+            uservalues:{}
+        }
+        var data = await axios.post("http://localhost/post", userobj);
+
+        var dbusers = JSON.parse(uservalues.data.body).data;
+        setUsers(dbusers);
+    }
+    useEffect(()=>{
+        ReadUsers();
+    }, [])
+
     return (
         <View style={styles.LoginPage}>
             <Image
@@ -31,7 +64,10 @@ function Register(){
             placeholder="Password"
             secureTextEntry={true}></TextInput>
 
-            <TouchableOpacity style={styles.NewRegistrationButton}>
+            <TouchableOpacity style={styles.NewRegistrationButton}
+            onPress={()=>{
+                CreateUser();
+            }}>
                 <Text style={styles.RegisterButtonText}> Create Account </Text>
             </TouchableOpacity>
             <TouchableOpacity 
