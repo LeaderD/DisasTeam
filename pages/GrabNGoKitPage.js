462f54bf51e67772dd0411d2f1d26721e6327a42
LucaMattia <lucamat1997@gmail.com>
2019-11-13 20:34:11 -0800

    Added everyting to each kit page

---
+++ pages/GrabNGoKitPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import GrabNGoKit from '../comps/GrabNGoKit';
+
+function GrabNGoKitPage(){
+    return (
+     <View>
+         <GrabNGoKit />
+     </View>
+    )
+ };
+ 
+ export default GrabNGoKitPage;
---
+++ pages/WorkKitPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import WorkKit from '../comps/WorkKit'
+
+function WorkKitPage(){
+    return (
+     <View>
+         <WorkKit />
+     </View>
+    )
+ };
+ 
+ export default WorkKitPage;
